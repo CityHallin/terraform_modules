817725703cef9ddda0788b32a92af6d60b68ab10
cityhallin <cityhallin.com>
2024-03-15 16:26:01 -0600

    re-add zip

--- solutions/cloud_resume_api_challenge/_module_general.tf
+++ solutions/cloud_resume_api_challenge/_module_general.tf
@@ -1,3 +1,9 @@
+#Prep ZIP file function Deployment
+data "archive_file" "compress_function_files" {
+  type        = "zip"
+  source_dir  = "../function"
+  output_path = "../files/function.zip"
+}
 
 #Get Azure tenant and environment info
 data "azurerm_client_config" "current" {}
